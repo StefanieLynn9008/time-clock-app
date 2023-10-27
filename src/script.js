a82d68c77b3078e0be07128c309fe0226b9c1a78
Stefanie M <stefanielynn9008@gmail.com>
2023-10-27 10:16:42 -0700

    Updated JS elements and added 4 functional timezones

--- src/script.js
+++ src/script.js
@@ -1,1 +1,44 @@
+function updateTime() {
+  // Los Angeles Time
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
 
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = `${losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  )}`;
+
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = `${sydneyTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  )}`;
+
+  let dublinElement = document.querySelector("#dublin");
+  let dublinDateElement = dublinElement.querySelector(".date");
+  let dublinTimeElement = dublinElement.querySelector(".time");
+  let dublinTime = moment().tz("Europe/Dublin");
+
+  dublinDateElement.innerHTML = dublinTime.format("MMMM Do YYYY");
+  dublinTimeElement.innerHTML = `${dublinTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  )}`;
+
+  let bangkokElement = document.querySelector("#bangkok");
+  let bangkokDateElement = bangkokElement.querySelector(".date");
+  let bangkokTimeElement = bangkokElement.querySelector(".time");
+  let bangkokTime = moment().tz("Asia/Bangkok");
+
+  bangkokDateElement.innerHTML = bangkokTime.format("MMMM Do YYYY");
+  bangkokTimeElement.innerHTML = `${bangkokTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  )}`;
+}
+
+setInterval(updateTime, 1000);
